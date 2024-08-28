cd25f6b4902d80a2812104b13d0553dddc011e1c
Ashwin <ashwinvx@users.noreply.github.com>
2024-08-28 13:02:28 -0400

    updated insertion sort

--- insertion.js
+++ insertion.js
@@ -1,11 +1,8 @@
 function insertionSort(arr) {
-    let j, temp;
     for(let i = 1; i < arr.length; i++){
-        j = i - 1;
+        let j = i - 1;
         while(j>=0 && arr[j+1] < arr[j]){
-            temp = arr[j+1];
-            arr[j+1] = arr[j];
-            arr[j] = temp;
+            [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
             j--;
         }   
     }
