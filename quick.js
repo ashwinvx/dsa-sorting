c4575cf4c80c8ac96262a1855ff27f9faa09f9f2
Ashwin <ashwinvx@users.noreply.github.com>
2024-08-30 09:39:58 -0400

    updated quick sort

--- quick.js
+++ quick.js
@@ -3,29 +3,32 @@ pivot accepts an array, starting index, and ending index
 You can assume the pivot is always the first element
 */
 
-function pivot(arr, s, e){
-    return arr[e];
+
+function pivot(arr, s=0, e=arr.length-1){
+    let pivot = arr[s];
+    let left = s;
+    for(let i = s; i <= e; i++){
+        if(arr[i] < pivot){
+            left++;
+            [arr[left], arr[i]] = [arr[i], arr[left]];
+        }
+    }
+    arr[s] = arr[left];
+    arr[left] = pivot;
+    return left;
 }
 
 /*
 quickSort accepts an array, left index, and right index
 */
 
-function quickSort(arr, s, e) {
+function quickSort(arr, s=0, e=arr.length-1) {
     if(e-s <= 0) return arr;
 
-    let pivot = this.pivot(arr, s, e);
-    let left = s;
-    for(let i = s; i <= e; i++){
-        if(arr[i] < pivot){
-            [arr[left], arr[i]] = [arr[i], arr[left]];
-            left++;
-        }
-    }
-    arr[e] = arr[left];
-    arr[left] = pivot;
-    this.quickSort(arr, s, left-1);
-    this.quickSort(arr,left+1, e);
+    let pivotIndex = pivot(arr, s, e);
+    //console.log('pivot-->',arr[pivotIndex]);
+    quickSort(arr, s, pivotIndex-1);
+    quickSort(arr,pivotIndex+1, e);
     return arr;
 }
-module.exports = quickSort;
+module.exports = {pivot, quickSort};
